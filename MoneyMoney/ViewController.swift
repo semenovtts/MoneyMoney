cc7911e56379760d0f65ea52aa1b6ffcc595f07b
Evgeny Semenov <semenov.e.vya@sberbank.ru>
2020-02-05 15:34:02 +0300

    Initial Commit

---
+++ MoneyMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyMoney
+//
+//  Created by Evgeny Semenov on 05/02/2020.
+//  Copyright © 2020 Evgeny Semenov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
